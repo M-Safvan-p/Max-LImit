299934f35aa85c716a106b134ce0e4ca3c5edbc8
safvan <muhammedsafvanp96@gmail.com>
2026-04-22 19:43:24 +0530

    add one generator qs

--- js/functions/generator.js
+++ js/functions/generator.js
@@ -1,15 +1,61 @@
-function* generator(){
-    console.log("start");
-    yield "hellow";
-    console.log("2 started");
-    yield "how are you";
-    console.log("last one");
-    yield "fine"
-    console.log("End!!!!")
+/*
+===========================================
+Question:
+Understand execution flow of a generator function.
+===========================================
+*/
+
+function* generator() {
+  console.log("start");
+  yield "hello";
+
+  console.log("2 started");
+  yield "how are you";
+
+  console.log("last one");
+  yield "fine";
+
+  console.log("End!!!!");
 }
 
-let start = generator()
-console.log(start.next().value)
-console.log(start.next().value)
-console.log(start.next().value)
-start.next()
+// Example
+// let gen = generator();
+
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// gen.next();
+
+
+/*
+===========================================
+Question:
+Generate numbers from 1 to n using a generator.
+===========================================
+*/
+
+function* generateNumbers(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i)
+    yield
+  }
+}
+
+// Example
+// let gen = generateNumbers(3);
+// gen.next();
+// gen.next();
+// gen.next();
+
+
+function* generateNumbers2(n) {
+  for (let i = 1; i <= n; i++) {
+    yield i;
+  }
+}
+
+// Example
+// let gen2 = generateNumbers2(5);
+// console.log(gen2.next().value);
+// console.log(gen2.next().value);
+// console.log(gen2.next().value);
