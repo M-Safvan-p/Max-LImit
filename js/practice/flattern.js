22d3a8ad8affa5e1008c85318703e55a39182b6f
safvan <muhammedsafvanp96@gmail.com>
2026-05-07 10:03:26 +0530

    add flattern code for array and object

--- js/practice/flattern.js
+++ js/practice/flattern.js
@@ -1,42 +1,67 @@
-// flatten array
+/*
+===========================================
+Question:
+Flatten a nested array using recursion.
+===========================================
+
+Flatten Array:
+- Convert nested array into single array
+- Remove all levels of nesting
+*/
 
 function flatten(array) {
-    let result = [];
+  let result = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if (Array.isArray(array[i])) {
-            result = result.concat(flatten(array[i]));
-        } else {
-            result.push(array[i]);
-        }
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result.push(...flatten(array[i]));
+    } else {
+      result.push(array[i]);
     }
-    return result
+  }
+
+  return result;
 }
 
+// Example
 console.log(flatten([1, [2, [3, [4, [5, [6, [7, [8, [9, [10]]]]]]]]]]));
 
-// flatten object 
+/*
+===========================================
+Question:
+Flatten a nested object using recursion.
+===========================================
 
-function obFlatten(obj, parentKey = "", result = {}) {
-    for (let key in obj) {
-        let newKey = parentKey ? parentKey + "." + key : key;
-        if (typeof obj[key] === "object" && obj[key] !== null) {
-            obFlatten(obj[key], newKey, result);
-        } else {
-            result[newKey] = obj[key]
-        }
+Flatten Object:
+- Convert nested object into single level object
+- Nested keys become dot notation
+*/
+
+function flattenObject(obj, parentKey = "", result = {}) {
+  for (let key in obj) {
+    let newKey = parentKey ? parentKey + "." + key : key;
+
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      flattenObject(obj[key], newKey, result);
+    } else {
+      result[newKey] = obj[key];
     }
-    return result
+  }
+
+  return result;
 }
+
+// Example
 const data = {
-    a: 1,
-    b: {
-        c: 2,
-        d: {
-            f: 3,
-            g: 7,
-        },
-        e:4
-    }
+  a: 1,
+  b: {
+    c: 2,
+    d: {
+      f: 3,
+      g: 7,
+    },
+    e: 4,
+  },
 };
-console.log(obFlatten(data));
+
+console.log(flattenObject(data));
